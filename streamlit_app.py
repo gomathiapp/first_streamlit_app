2065d0c7af8b6af5d5c0128d2e14b02786263490
gomathiapp <131616856+gomathiapp@users.noreply.github.com>
2023-04-23 16:18:11 -0400

    Update streamlit_app.py
    added header and text content

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.title('hello world')
+streamlit.header('first header for menu details')
+streamlit.text('Omega 3 Bluberry oatmeal')
+streamlit.text('strawberry smoothy ')
+streamlit.text('boiled egg ')
