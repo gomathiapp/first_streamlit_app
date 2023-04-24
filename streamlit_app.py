814cee589299c71470fac105fed05c1b31b94a3f
gomathiapp <131616856+gomathiapp@users.noreply.github.com>
2023-04-24 15:58:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,10 +22,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 import requests
 streamlit.header('Fruityvice Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 #streamlit.text(fruityvice_response.json())
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 # normalize the json data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display data in table format
 streamlit.dataframe(fruityvice_normalized)
+
+
