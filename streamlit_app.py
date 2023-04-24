c18bee4357474bc244c45fa059238e8aaea5919d
gomathiapp <131616856+gomathiapp@users.noreply.github.com>
2023-04-23 23:10:25 -0400

    Update streamlit_app.py
    import panda pythan lib to read a csv file and display using streamlit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 
 import streamlit
+import pandas
 streamlit.title('My parents new healthy dinner')
 streamlit.title('hello world')
 streamlit.header('first header for menu details')
@@ -8,3 +9,6 @@ streamlit.text(' 🥗 strawberry smoothy ')
 streamlit.text(' 🐔 boiled egg ')
 streamlit.text(' 🥑avacodo toast ')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
