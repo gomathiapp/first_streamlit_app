7a548c16ec38dcef9e4c80a1ce7432d9b5608a20
gomathiapp <131616856+gomathiapp@users.noreply.github.com>
2023-04-24 23:03:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,15 +27,25 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 #streamlit.text(fruityvice_response.json())
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+try
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+ if not fruit_choice:
+   streamlit.error("Please select a fruit to get information")
+ else
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+  # normalize the json data
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  # display data in table format
+  streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
-# normalize the json data
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display data in table format
-streamlit.dataframe(fruityvice_normalized)
+ #streamlit.write('The user entered ', fruit_choice)
+
+
+
 
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
